b27f1f2be133398e3045651693e43e8e1a83af41
andre21cavalcante <andre.cavalcantejr21@gmail.com>
2022-02-08 23:24:00 -0300

    Perguntas finalizadas

--- src/PerguntasSQL/pergunta-andre.sql
+++ src/PerguntasSQL/pergunta-andre.sql
@@ -1,7 +1,7 @@
-Quais jogadores mais pontuaram?
+--Quem são os 10 jogadores que mais pontuaram ?
 
 SELECT nome_jogador, sum(qtd_cestas_feitas) AS cestinhas FROM detalhes_jogo GROUP BY nome_jogador ORDER BY cestinhas DESC LIMIT 10;
 
-Quais jogadores mais deram assistências?
+--Quem são os 10 jogadores que mais deram assistências ?
 
 SELECT nome_jogador, sum(qtd_ast) AS assist FROM detalhes_jogo GROUP BY nome_jogador ORDER BY assist desc limit 10;
